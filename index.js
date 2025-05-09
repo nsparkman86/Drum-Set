aa2402ce05f322fe6d22ccb8ae4a999a72461066
Nathan Sparkman <nsparkman@live.com>
2025-05-08 23:04:54 -0500

    Add javascript code

---
+++ index.js
@@ -0,0 +1,8 @@
+for(let i=0; i<document.querySelectorAll(".drum").length; i++) {
+
+document.querySelectorAll("button")[i].addEventListener("click", clicky)};
+
+function clicky () {
+    
+    alert("hello")
+};
