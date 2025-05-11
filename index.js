d1414b1da513b8bf92bd347c58669e1001cd3c26
Nathan Sparkman <nsparkman@live.com>
2025-05-11 10:26:09 -0500

    Add key strokes

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ for(let i=0; i<document.querySelectorAll(".drum").length; i++) {
 
 document.querySelectorAll("button")[i].addEventListener("click", clicky)};
 document.addEventListener("keydown", clicky)
-function clicky () {
-    switch(this.innerHTML) {
+function clicky (event) {
+    switch(true) {
         
-        case "w" :
+        case (this.innerHTML === "w" || event.key === "w") :
         var kick = new Audio("./sounds/kick-bass.mp3");
         kick.play();
         break;
-        case "a" :
+        case (this.innerHTML === "a" || event.key === "a") :
         var snare = new Audio("./sounds/snare.mp3");
         snare.play();
         break; 
-        case "s" :
+        case (this.innerHTML === "s" || event.key === "s") :
             var tom1 = new Audio("./sounds/tom-1.mp3");
             tom1.play();
         break;
 
         default: console.log(this.innerHTML);
     }
-};
+}; sdfsdf
